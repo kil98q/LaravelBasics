9447bf310aba9559ef4c3e3ef6e8dd42e9645de7
Robin Jongmans <robin.jongmans.work@gmail.com>
2023-01-30 02:21:19 +0900

    changes

---
+++ database/factories/DepartmentFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Department;
+use App\Models\Employee;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Employee>
+ */
+class EmployeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'department_id' => Department::factory(),
+            //
+            'employee_name' => fake()->firstNameMale(),
+        ];
+    }
+}
